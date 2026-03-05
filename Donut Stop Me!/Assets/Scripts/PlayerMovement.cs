32484050ae781b37171e2547e88dcd313422b956
Cae <caeleymhenderson@gmail.com>
2026-03-05 14:07:47 -0500

    PLAYER MOVEMENT!!! camera needs fixing

--- Donut Stop Me!/Assets/Scripts/PlayerMovement.cs
+++ Donut Stop Me!/Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public PlayerInput playerInput;
+    private InputAction moveAction;
+
+    [SerializeField]
+    float speed = 5f;
+
     void Start()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Move");
+       
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        Vector2 direction = moveAction.ReadValue<Vector2>();
+        transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
     }
 }
