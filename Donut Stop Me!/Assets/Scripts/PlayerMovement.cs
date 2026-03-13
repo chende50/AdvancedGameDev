258662e162f07aa7800d4318937fe06407e2b5ac
Cae <caeleymhenderson@gmail.com>
2026-03-13 14:20:42 -0400

    Some animator stuff

--- Donut Stop Me!/Assets/Scripts/PlayerMovement.cs
+++ Donut Stop Me!/Assets/Scripts/PlayerMovement.cs
@@ -20,12 +20,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
-    }
-
-    void MovePlayer()
-    {
         Vector2 direction = moveAction.ReadValue<Vector2>();
         transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
     }
+
 }
