d4c11824f51eda1037992af4eaa094d1e0a1dedf
Cae <caeleymhenderson@gmail.com>
2026-03-15 14:41:30 -0400

    Camera fixed, animations started to be added

---
+++ Donut Stop Me!/Assets/Scripts/playerAnimator.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private InputAction moveAction;
+
+    void Awake()
+    {
+        var playerInput = GetComponent<PlayerInput>();
+        if (playerInput != null)
+        {
+            moveAction = playerInput.actions.FindAction("Move");
+            moveAction.Enable();
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        bool isRunning = moveAction.ReadValue<Vector2>().magnitude > 0.1f;
+
+        if (animator != null)
+        {
+            animator.SetBool("isRunning", isRunning);
+        }
+    }
+}
