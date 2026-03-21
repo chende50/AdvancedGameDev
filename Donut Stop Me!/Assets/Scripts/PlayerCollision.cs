c934bc114425846defc423f7a1a625304ad70bfd
BEEACREE <beeacree@gmail.com>
2026-03-21 11:05:43 -0400

    Feat: Player, Sawblade, and Spinning blade
    Player made into prefab, added player collision script, sawblade and spinning blade made collideable, and level pieces given colliders

---
+++ Donut Stop Me!/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    [SerializeField] static int playerHealth = 3;
+    static int donutsCollected = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Danger"))
+        {
+            playerHealth -= 1;
+            Debug.Log("Player touched danger. Player health: " +  playerHealth);
+        }
+        else if (collision.gameObject.CompareTag("Donut"))
+        {
+            donutsCollected += 1;
+            Debug.Log("Player touched donut. Donuts collected: " + donutsCollected);
+        }
+    }
+}
