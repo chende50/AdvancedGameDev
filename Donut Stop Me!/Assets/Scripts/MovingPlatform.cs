f5c44218a3e606acd9c1744dafc0992ecf8fedbf
Cae <caeleymhenderson@gmail.com>
2026-05-08 17:40:23 -0400

    Create Level 3

--- Donut Stop Me!/Assets/Scripts/MovingPlatform.cs
+++ Donut Stop Me!/Assets/Scripts/MovingPlatform.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    [SerializeField]
+    public string direction; // "x" or "z"
     public float distance = 5f; // How far it moves from the start
     public float speed = 2f;    // How fast it moves
 
@@ -18,9 +20,22 @@ public class MovingPlatform : MonoBehaviour
         // PingPong returns a value that moves back and forth
         float movement = Mathf.PingPong(Time.time * speed, distance);
 
-        // Update the platform's X position
-        transform.position = new Vector3(startPos.x, startPos.y, startPos.z + movement);
+        //Update based on direction given
+        if (direction == "x")
+        {
+            transform.position = new Vector3(startPos.x, startPos.y, startPos.z + movement);
+
+        }
+        else if (direction == "z")
+        {
+            transform.position = new Vector3(startPos.x + movement, startPos.y, startPos.z);
+        }
+        else
+        {
+            Debug.LogError("Invalid direction for MovingPlatform. Use 'x' or 'z'.");
+        }
     }
+
 
     private void OnCollisionEnter(Collision collision)
     {
